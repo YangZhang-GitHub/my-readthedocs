8307512d5a62e87b4e8c6065c169d4dcb3a53340
YangZhang-GitHub <2533471770@qq.com>
2021-01-19 11:43:08 +0800

    first commit

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+language = 'zh_CN'
+locale_dirs = ['../po']
 
 # -- Options for HTML output -------------------------------------------------
 
